fa3a39af30e6d08ba7c3583f129b669288cb97dd
easyintegration-blockchain <easyintegration.blockchain@gmail.com>
2022-07-15 18:15:47 +0000

    Initial Draft

---
+++ src/stories/App.stories.tsx
@@ -0,0 +1,12 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import App from '../components/App';
+
+export default {
+    title: 'EasyIntegration',
+    component: App
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App />;
+
+export const WelcomeApp = Template.bind({});
+WelcomeApp.args = {};
